d9c990a4ea7838859a04c99035d34ab6f02ca629
Anders Sandström <anders.sandstrom@esss.se>
2020-03-25 20:15:46 +0100

    Minor update..

--- ecmcPlugin_Advanced-loc/ecmcPlugin_AdvancedApp/src/ecmcPluginExample.c
+++ ecmcPlugin_Advanced-loc/ecmcPlugin_AdvancedApp/src/ecmcPluginExample.c
@@ -22,22 +22,26 @@ extern "C" {
 #include "ecmcPluginDefs.h"
 #include "ecmcAdvanced.h"
 
+static int    lastEcmcError  = 0;
+static double ecmcSampleRate = 0;
+static void*  ecmcAsynPort   = NULL;
+
 /** Optional. 
  *  Will be called once just before ecmc goes into realtime mode.
  *  Return value other than 0 will be considered error.
  **/
-int exampleConstruct(void)
+int adv_exampleConstruct(void)
 {
-  printf("exampleConstruct...\n");
+  printf("adv_exampleConstruct...\n");
   return 0;
 }
 
 /** Optional function.
  *  Will be called once at unload.
  **/
-void exampleDestruct(void)
+void adv_exampleDestruct(void)
 {
-  printf("exampleDestruct...\n");
+  printf("adv_exampleDestruct...\n");
 }
 
 /** Optional function.
@@ -46,9 +50,10 @@ void exampleDestruct(void)
  *  this plugin to react on ecmc errors
  *  Return value other than 0 will be considered error.
  **/
-int exampleRealtime(int ecmcError)
+int adv_exampleRealtime(int ecmcError)
 {
-  printf("exampleRealtime...\n");
+  printf("adv_exampleRealtime...\n");
+  lastEcmcError = ecmcError;
   return 0;
 }
 
@@ -57,11 +62,13 @@ int exampleRealtime(int ecmcError)
  *  Return value other than 0 will be considered error.
  *  ecmcRefs is only valid between "exampleEnterRT()" and "exampleExitRT()"
  **/
-int exampleEnterRT(void* ecmcRefs){
+int adv_exampleEnterRT(void* ecmcRefs){
   // Determine ecmc sample rate
-  printf("Ecmc sample rate is: %lf ms",getSampleRate(ecmcRefs));
+  ecmcSampleRate = getSampleRate(ecmcRefs);
+  printf("Ecmc sample rate is: %lf ms",ecmcSampleRate);
+
   // Use ecmcAsynPort
-  useAsynPort(ecmcRefs);
+  ecmcAsynPort = getAsynPort(ecmcRefs);
   return 0;
 }
 
@@ -69,22 +76,22 @@ int exampleEnterRT(void* ecmcRefs){
  *  Will be called once just before leaving realtime mode
  *  Return value other than 0 will be considered error.
  **/
-int exampleExitRT(void){
-  printf("exampleExitRT...\n");
+int adv_exampleExitRT(void){
+  printf("adv_exampleExitRT...\n");
   return 0;
 }
 
 /** Optional plc function*/
-double customPlcFunc1(double arg1, double arg2)
+double adv_customPlcFunc1(double arg1, double arg2)
 {
-  printf("customPlcFunc1 %lf, %lf.\n",arg1,arg2);
+  //printf("adv_customPlcFunc1 %lf, %lf.\n",arg1,arg2);
   return arg1 * arg2;
 }
 
 /** Optional plc function*/
-double customPlcFunc2(double arg1, double arg2, double arg3)
+double adv_customPlcFunc2(double arg1, double arg2, double arg3)
 {
-  printf("customPlcFunc2 %lf, %lf, %lf.\n",arg1,arg2,arg3);
+  //printf("adv_customPlcFunc2 %lf, %lf, %lf.\n",arg1,arg2,arg3);
   return arg1 * arg2 * arg3;
 }
 
@@ -92,26 +99,30 @@ double customPlcFunc2(double arg1, double arg2, double arg3)
 struct ecmcPluginData pluginDataDef = {
   // Name 
   .name = "ecmcExamplePlugin",
+  // Description
+  .desc = "Advanced example with use of asynport obj.",
   // Plugin version
   .version = ECMC_EXAMPLE_PLUGIN_VERSION,
   // ECMC_PLUG_VERSION_MAGIC
   .ifVersion = ECMC_PLUG_VERSION_MAGIC, 
   // Optional construct func, called once at load. NULL if not definded.
-  .constructFnc = exampleConstruct,
+  .constructFnc = adv_exampleConstruct,
   // Optional destruct func, called once at unload. NULL if not definded.
-  .destructFnc = exampleDestruct,
+  .destructFnc = adv_exampleDestruct,
   // Optional func that will be called each rt cycle. NULL if not definded.
-  .realtimeFnc = exampleRealtime,
+  .realtimeFnc = adv_exampleRealtime,
   // Optional func that will be called once just before enter realtime mode
-  .realtimeEnterFnc = exampleEnterRT,
+  .realtimeEnterFnc = adv_exampleEnterRT,
   // Optional func that will be called once just before exit realtime mode
-  .realtimeExitFnc = exampleExitRT,
+  .realtimeExitFnc = adv_exampleExitRT,
 
-  // Allow max ECMC_PLUGIN_MAX_FUNC_COUNT custom funcs
+  // Allow max ECMC_PLUGIN_MAX_FUNC_COUNT custom plc funcs
   .funcs[0] =      
       { /*----customPlcFunc1----*/
         // Function name (this is the name you use in ecmc plc-code)
-        .funcName = "ex_plugin_func_1",
+        .funcName = "adv_plugin_func_1",
+        // Function description
+        .funcDesc = "Multiply arg0 with arg1.",
         // Number of arguments in the function prototytpe
         .argCount = 2,
         /**
@@ -121,16 +132,18 @@ struct ecmcPluginData pluginDataDef = {
         **/
         .funcArg0 = NULL,
         .funcArg1 = NULL,
-        .funcArg2 = customPlcFunc1, // Func 1 has 2 args
+        .funcArg2 = adv_customPlcFunc1, // Func 1 has 2 args
         .funcArg3 = NULL,
         .funcArg4 = NULL,
         .funcArg5 = NULL,
         .funcArg6 = NULL
       },
-    .funcs[1] =
+  .funcs[1] =
       { /*----customPlcFunc2----*/
         // Function name (this is the name you use in ecmc plc-code)
-        .funcName = "ex_plugin_func_2",
+        .funcName = "adv_plugin_func_2",
+        // Function description
+        .funcDesc = "Multiply arg0, arg1 and arg2.",
         // Number of arguments in the function prototytpe
         .argCount = 3,
         /**
@@ -141,12 +154,26 @@ struct ecmcPluginData pluginDataDef = {
         .funcArg0 = NULL,
         .funcArg1 = NULL,
         .funcArg2 = NULL,
-        .funcArg3 = customPlcFunc2, // Func 2 has 3 args
+        .funcArg3 = adv_customPlcFunc2, // Func 2 has 3 args
         .funcArg4 = NULL,
         .funcArg5 = NULL,
         .funcArg6 = NULL
       },
-      .funcs[2] = {0} //last element set all to zero..
+  .funcs[2] = {0}, //last element set all to zero..
+
+  /** Plugin specific constants (add prefix to not risc collide with 
+   * other modules) */
+  .consts[0] = {
+        .constName = "adv_CONST_1",
+        .constDesc = "Test constant \"adv_CONST_1\" = 1.234567890",
+        .constValue = 1.234567890,
+      },
+   .consts[1] = {
+        .constName = "adv_CONST_2",
+        .constDesc = "Test constant \"adv_CONST_2\" = 9.876543210",
+        .constValue = 9.876543210,
+      },
+  .consts[2] = {0},
 };
 
 ecmc_plugin_register(pluginDataDef);
